43c5978737bc0f18bf87438ecc03a770e11d08fa
PhongChillFansub <nguyen030621@gmail.com>
2026-03-19 23:55:28 +0700

    a0.0 mar 19 2026

--- moves.lua
+++ moves.lua
@@ -1,4 +1,5 @@
-script_name = "[Level 2] typing_fx"
+script_name = "[Level 2] moves"
 script_description = "[Phòng Chill Fansub] Effect di chuyển quỹ đạo cong bezier (\\moves) với VSFilter (không dùng VSFilterMod)"
 script_author = "Phòng Chill Fansub"
 script_version = "1.0"
+--[[alpha 0.0 19/3/2026]]
