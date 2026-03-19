aee593067dc813f199d071bd8ff250fb9da1d03a
PhongChillFansub <nguyen030621@gmail.com>
2026-03-19 21:39:12 +0700

    2a

--- test.lua
+++ test.lua
@@ -51,7 +51,7 @@ function quad2cubeBezier(qp0,qp1,qp2)
 	local cp0, cp3 = tblcpy(qp0), tblcpy(qp2)
 	for plane=1,2 do
 		cp1[plane]=cp0[plane]+2/3*(qp1[plane]-qp0[plane])
+		cp2[plane]=qp2[plane]+2/3*(qp1[plane]-qp2[plane])
 	end
-
 	return cp0,cp1,cp2,cp3 
 end
