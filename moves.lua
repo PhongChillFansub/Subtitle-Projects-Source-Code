ee872de225a279955ce689423bd4cf03f9b3e7e2
PhongChillFansub <nguyen030621@gmail.com>
2026-03-20 21:48:00 +0700

    beta 1.06 mar 20 2026

--- moves.lua
+++ moves.lua
@@ -2,7 +2,7 @@ script_name = "[Level 2] moves"
 script_description = "[Phòng Chill Fansub] Effect di chuyển quỹ đạo cong bezier (\\moves) với VSFilter (không dùng VSFilterMod)"
 script_author = "Phòng Chill Fansub"
 script_version = "1.0"
---[[beta 1.05, 20/3/2026]]
+--[[beta 1.06, 20/3/2026]]
 
 function q2cBezier(qp0,qp1,qp2)
 	--[[Hàm biến đổi tọa độ (2d) đường cong Bezier cấp 2 thành cấp 3 (để trực quan bằng lệnh vẽ)]]
@@ -104,7 +104,7 @@ function moves3(segments,x1,y1,x2,y2,x3,y3,t0,t1)
 		return _G.interpolate(x,t0,t1)
 	end
 	local cnf0 = function(x)
-		return _G.tonumber(_G.string.format('%.0f',x))
+		return _G.tonumber(_G.string.format('%.0f',x*(t1-t0)))
 	end
 	moves3_data = {xi={},yi={},ti={},i=bezier_approx(cp0,cp1,cp2,cp3,segments)}
 	--[[moves3_data: xi, yi, ti, i: tọa độ x,y, thời gian tại điểm i (0..1)]]
