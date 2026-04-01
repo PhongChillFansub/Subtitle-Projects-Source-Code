c3eaa7101652998589da6b44bc9410947b75410e
PhongChillFansub <nguyen030621@gmail.com>
2026-04-01 15:47:49 +0700

    v3.2 beta 1.03 apr 1 2026

--- funcdraw_shapelib.lua
+++ funcdraw_shapelib.lua
@@ -2,7 +2,7 @@ script_name = "[Level 2] funcdraw"
 script_description = "[Phòng Chill Fansub] Thư viện hình vẽ bằng lệnh vẽ của funcdraw fx"
 script_author = "Phòng Chill Fansub"
 script_version = "3.2"
---[[v3.2 beta 1.01 1/4/2026. Bổ sung 2 hình vẽ nữa của tệp phát triển cũ]]
+--[[v3.2 beta 1.03 1/4/2026. Thêm các hình của pj 45a, 45c]]
 --[[Sử dụng funcdraw v3.2 beta 1 (di chuyển lên GitHub.)]]
 --[[Tương đương lib 2 của tệp phát triển funcdraw v3.1.]]
 
@@ -134,3 +134,65 @@ ring1 = function(j0,maxj0)
     },aconv(-21,1) ) 
 end
 --[[Hình ring1 của tệp phát triển v3.1]]
+
+function moon1(j0,maxj0) 
+    if j0==nil or maxj0==nil or j0<0 then 
+        return nil 
+    end 
+    local startingPoint = {-45,-120} 
+    local ease = {1-(1-_G.clamp(j0/maxj0,0,1))^2.8} 
+    local movingOffset = {-1*startingPoint[1]*(-1+ease[1] ),-1*startingPoint[2]*(-1+ease[1])} 
+    return { 
+        rotate(
+            aconv(-7,1),
+            {-13+movingOffset[1],-63+movingOffset[2]},
+            { 
+                fd3m(-13-194+movingOffset[1],-63+movingOffset[2]),
+                circleRad(194,math.rad(-180),math.rad(-257)) 
+            }
+        ), 
+        fd3n(_G.table.unpack(findPos(
+            -66+movingOffset[1],
+            -114+movingOffset[2],
+            148,
+            math.rad(-62)
+        ))), 
+        circleRad(148,math.rad(-62),math.rad(214)) 
+    } 
+end
+--[[moon1 của pj 45a]]
+
+function ring1(j0,maxj0) 
+    if j0==nil or maxj0==nil or j0<0 then 
+        return nil 
+    end 
+    local ease = {1-(1-_G.clamp(j0/maxj0,0,1))^1.7} 
+    local angleRange = {232,360} 
+    return stretch(0,1.9,{0,0},{ 
+        fd3m(_G.table.unpack(findPos(
+            0,
+            -2,
+            149,
+            math.rad(-55-angleRange[1]-(angleRange[2]-angleRange[1])*ease[1]),
+        ))), 
+        circleRad(
+            149,
+            math.rad(-55-angleRange[1]-(angleRange[2]-angleRange[1])*ease[1]),
+            math.rad(angleRange[1]+(angleRange[2]-angleRange[1])*ease[1])
+        ), 
+        fd3n(_G.table.unpack(findPos(
+            0,
+            0,
+            165,
+            math.rad(-55)
+        ))), 
+        circleRad(
+            165,
+            math.rad(-55),
+            math.rad(-angleRange[1]-(angleRange[2]-angleRange[1])*ease[1])
+        )
+    },
+    math.rad(-21) ) 
+end
+--[[ring1 của pj 45a]]
+
